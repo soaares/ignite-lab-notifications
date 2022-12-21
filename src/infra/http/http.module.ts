cf0f22ab1b80dba1aede6cc2252585c1e1695fd5
Raphael Oliveira Soares <raphael.soares@datametrica.com.br>
2022-12-21 17:47:25 -0300

    feature: insert providers at http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,11 +2,23 @@ import { Module } from "@nestjs/common";
 import { SendNotification } from "@domain/use-cases/send-notification";
 import { DatabaseModule } from "../database/database.module";
 import { NotificationsController } from "./controllers/notifications.controller";
+import { CancelNotification } from "@domain/use-cases/cancel-notification";
+import { GetRecipientNotifications } from "@domain/use-cases/get-recipient-notifications";
+import { CountRecipientNotifications } from "@domain/use-cases/count-recipient-notifications";
+import { ReadNotification } from "@domain/use-cases/read-notification";
+import { UnreadNotification } from "@domain/use-cases/unread-notification";
 
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationsController],
-    providers: [SendNotification]
+    providers: [
+        SendNotification,
+        CancelNotification,
+        GetRecipientNotifications,
+        CountRecipientNotifications,
+        ReadNotification,
+        UnreadNotification
+    ]
 })
 
-export class HttpModule {}
+export class HttpModule { }
