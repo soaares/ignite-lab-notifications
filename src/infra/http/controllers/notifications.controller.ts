0091389f625972923b6f476c7c6ea037c4fc3a50
Raphael Oliveira Soares <raphael.soares@datametrica.com.br>
2022-12-21 15:29:51 -0300

    feature: add cancel controller to notifications controller

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Patch, Param } from '@nestjs/common';
 import { SendNotification } from '@domain/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { NotificationViewModel } from '../view-models/notification-view-model';
+import { CancelNotification } from '@domain/use-cases/cancel-notification';
 
 @Controller()
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) { }
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification
+  ) { }
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') notificationId: string) {
+    await this.cancelNotification.execute({ notificationId })
+  }
+
+  async countFromRecipient() { }
+
+  async getFromRecipient() { }
+
+  async read() { }
+
+  async unread() { }
 
   @Post('notifications')
   async create(@Body() body: CreateNotificationBody) {
